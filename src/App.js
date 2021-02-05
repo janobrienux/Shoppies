24fd626188332cd72ae756111c091ab7bdefd236
January <januaryobrien.ux@gmail.com>
2021-02-05 10:33:07 -0500

    updated styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css";
 
 import Search from "./components/Search.js";
 import Results from "./components/Results.js";
--- src/components/Results.js
+++ src/components/Results.js
@@ -3,7 +3,7 @@ import React from "react";
 const Results = ({ searchTerm, searchResults, error, setNominations, nominations }) => {
   const handleNomination = (newNomination) => {
     if (nominations.length === 5) {
-      alert("Unfortunately can only nominate 5 films!");
+      alert("Unfortunately you can only nominate 5 films!");
     } else {
       const updatedNominations = [...nominations, newNomination];
       setNominations(updatedNominations);
@@ -18,15 +18,19 @@ const Results = ({ searchTerm, searchResults, error, setNominations, nominations
   return (
     <section className="results">
       <h2>Results</h2>
-      <p>{error ? `${error}` : `Results for "${searchTerm}"`}</p>
+      <p className="results">{error ? `${error}` : `Results for "${searchTerm}"`}</p>
       <ul>
         {searchResults.map((result) => {
           return (
-            <li key={result.imdbID}>
-              {result.Title} ({result.Year})
-              <button disabled={checkNomination(result.imdbID)} onClick={() => handleNomination(result)}>
-                Nominate
-              </button>
+            <li className="d-inline-flex" key={result.imdbID}>
+              <div className="card ">
+                <h5 className="card-title">{result.Title}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{result.Year}</h6>
+                <img alt="test" src={result.Poster} />
+                <button disabled={checkNomination(result.imdbID)} onClick={() => handleNomination(result)}>
+                  Nominate
+                </button>
+              </div>
             </li>
           );
         })}
